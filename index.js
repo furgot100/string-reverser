4ebc16750ac1ec5da0710e3311cb9d094a41e343
Francis Tsang <frtsang40@gmail.com>
2021-05-12 09:32:05 -0700

    finished package

---
+++ index.js
@@ -0,0 +1,16 @@
+const ReverseString = (str) => {
+    const revArray = [];
+    const length = str.length - 1;
+    
+    for(let i = length; i >= 0; i--) {
+        revArray.push(str[i]);
+    }
+      
+    return revArray.join('');
+}
+
+// console.log(ReverseString('Hello'))
+
+module.exports = {
+    ReverseString
+}
